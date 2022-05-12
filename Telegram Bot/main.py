f25ac8b9a0ffe3efcc800567b7ebe1f83c72033f
javadick1 <Nikita.bodyaginsor@yandex.ru>
2022-05-12 14:32:32 +0300

    Bet1

--- Telegram Bot/main.py
+++ Telegram Bot/main.py
@@ -1,18 +1,16 @@
 # Создание бота и парсер для бота и новостей
-
 from email import message
+from lib2to3.pgen2 import token
 import telebot;
-bot = telebot.TeleBot('5159576994:AAHKFgJ38H0dtAZGms4YrbMjBGADrBwQF9E')
+token = "5159576994:AAHKFgJ38H0dtAZGms4YrbMjBGADrBwQF9E"
+id_channel = "1432747261"
+bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=['start']) ### CHANGE
+
+@bot.message_handler(content_types=['text']) ### CHANGE
 def get_txt_message(message): # more functionals in START, EJECT
-    if message == "Привет":
-        bot.send_message(message.from_user.id, "Привет, скоро начну отправлять тебе важные новости")
-    elif message == "/help":
-        bot.send_message(message.from_user.id, "Для взаимодействия со мной просто напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понял, напиши пожалуйста /help")
+    bot.send_message(id_channel, message.text)
 
 
-bot.polling(none_stop=True,interval=0)
+bot.polling(none_stop=True)
 
--- Telegram Bot/parser.py
+++ Telegram Bot/parser.py
@@ -1,4 +1,18 @@
 # New api and Parser
+import requests
+from bs4 import BeautifulSoup
+from yarl import URL
+
+URL = "https://riamo.ru/tag/743/moskva-reka-xl"
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content,"html.parser")
+
+post = soup.find("a", class_="card-horizontal--img-container")
+
+title = post.find("a", class_="card_horizontal__header").text.strip()
+description = post.find("div", class_="card_horizontal__description").text.strip()
+url = post.find("a", class_="card_horizontal__img-container", href=True)["href"].strip()
 
 
-
+print(title, description, url, sep="\n\n")
